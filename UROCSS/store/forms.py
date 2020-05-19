d2ef5767ce2fd0d26e6b66e69c92ef5e4423e710
Sudip Adh <xudip12@gmail.com>
2020-05-19 13:31:57 +0545

    error on black formatting setting fixed

--- UROCSS/store/forms.py
+++ UROCSS/store/forms.py
@@ -1,8 +1,5 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import (
-    UserCreationForm,
-    PasswordChangeForm,
-)
+
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
